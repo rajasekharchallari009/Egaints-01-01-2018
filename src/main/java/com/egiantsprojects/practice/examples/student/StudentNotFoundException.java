e81a1d0bcc0003c748cfb7ce069bbd1a94bc04df
rajasekharchallari009 <challariraj@gmail.com>
2019-07-18 23:24:58 -0400

    EgiantsTasks: Successfully completed project

---
+++ src/main/java/com/egiantsprojects/practice/examples/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.egiantsprojects.practice.examples.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into student
+values(10001,'sam', 'E1234567');
+
+insert into student
+values(10002,'ashley', 'A1234568');
+
+insert into student
+values(10003,'Ross', 'v1423696');
+
+insert into student
+values(10004,'math', 'E9689569');
